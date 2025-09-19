aeaa95bfd60d3ff0dca86be9a88e31466a7cf80b
SiruiZhang8 <xiaose832@gmail.com>
2025-09-20 09:58:20 +1000

    update

--- table.sql
+++ table.sql
@@ -25,6 +25,7 @@ create table if not exists issue
     description  text                                  not null comment 'issue description',
     location     varchar(512)                          not null comment 'issue location',
     status       varchar(20) default 'new'             not null comment 'status',
+    happen_time  datetime     default CURRENT_TIMESTAMP not null comment 'issue happen time',
     create_time  datetime     default CURRENT_TIMESTAMP not null comment 'create time',
     update_time  datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment 'update time'
 ) comment 'issue' collate = utf8mb4_unicode_ci;
