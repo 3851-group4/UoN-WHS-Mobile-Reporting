37fd99a695c1bff8c55fd4f5f641223f426486af
SiruiZhang8 <xiaose832@gmail.com>
2025-09-19 15:13:36 +1000

    create issue table

--- table.sql
+++ table.sql
@@ -16,3 +16,16 @@ create table if not exists user
     UNIQUE KEY uk_user_account (user_name),
     INDEX idx_user_name (user_name)
 ) comment 'user' collate = utf8mb4_unicode_ci;
+
+-- issue table
+create table if not exists issue
+(
+    id           bigint auto_increment comment 'id' primary key,
+    user_id      bigint                                not null comment 'user id',
+    title        varchar(100)                          not null comment 'issue title',
+    description  text                                  not null comment 'issue description',
+    location     varchar(512)                          not null comment 'issue location',
+    status       varchar(20) default 'new'             not null comment 'status',
+    create_time  datetime     default CURRENT_TIMESTAMP not null comment 'create time',
+    update_time  datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment 'update time'
+) comment 'issue' collate = utf8mb4_unicode_ci;
