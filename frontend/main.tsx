ab2269e499f0b5eb92c2f2264a9363589fb159be
zhiyeren003 <zhiyeren003@gmail.com>
2026-02-12 14:06:56 +1100

    Update main.tsx

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css"; // CSS Styling
 
 const container = document.getElementById("root");
 if (container) {
@@ -15,4 +14,3 @@ if (container) {
     </React.StrictMode>
   );
 }
-
